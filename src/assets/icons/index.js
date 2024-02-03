139e73693089b5690a7eb0862dc1d6e307cd5b97
Stepan Skichko <st.skichko@gmail.com>
2024-02-03 10:42:08 +0200

    add icons & some img // delete trash

---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import burgerMenu from './burger-menu.svg';
+import closeMenu from './close-burger.svg';
+import userTag from './tag-user.svg';
+import call from './call.svg';
+import sms from './sms.svg';
+
+export { burgerMenu, closeMenu, userTag, call, sms };
---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import balloons1 from './balloons1.png';
+import balloons2 from './balloons2.png';
+import logo from './logo.png';
+
+export { balloons1, balloons2, logo };
